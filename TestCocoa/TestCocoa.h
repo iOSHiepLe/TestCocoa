7889109179c1f8c82dc407b35d54916c661fba67
HiepLe <hieplemobile@gmail.com>
2022-08-23 15:13:45 +0700

    test wrap

--- TestCocoa/TestCocoa.h
+++ TestCocoa/TestCocoa.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double TestCocoaVersionNumber;
 FOUNDATION_EXPORT const unsigned char TestCocoaVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TestCocoa/PublicHeader.h>
-#import <Flutter/Flutter.h>
-#import <FlutterPluginRegistrant/FlutterPluginRegistrant.h>
